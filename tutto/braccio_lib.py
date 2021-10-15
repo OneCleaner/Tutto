5489184e8fd5a396936da77d1aa0ebba9d32a94e
Alby7503 <alberto.vona24@gmail.com>
2021-10-15 13:55:30 +0200

    miglioramenti
    da testare

--- tutto/braccio_lib.py
+++ tutto/braccio_lib.py
@@ -1,41 +1,77 @@
-from universal import move
-from time import sleep
-from adafruit_servokit import ServoKit
-import RPi.GPIO as gpio
-from time import sleep
+from time import sleep, time
 
-gpio.setmode(gpio.BCM)
-gpio.setup(12, gpio.OUT)
-gpio.setup(6, gpio.OUT)
+from RPi.GPIO import setmode, setup, output, BCM, HIGH, LOW, IN, OUT, PUD_DOWN
+from RPi.GPIO import input as gpio_input
+from adafruit_servokit import ServoKit
+
+from universal import move
+
+setmode(BCM)
 
 POSIZIONI = {
-	1 : 80,
-	15: 0,
-	8: 20,
-	3: 30
+    1: 80,
+    15: 0,
+    8: 20,
+    3: 30
 }
 
 KIT = ServoKit(channels=16)
-#PATH = "/dev/ttyUSB0"
+# PATH = "/dev/ttyUSB0"
+
 
 def braccio_raw(servo, angolo):
-	KIT.servo[servo].angle = angolo
+	"""Modifica l'angolo di un servo senza delay"""
+    KIT.servo[servo].angle = angolo
+
 
 def braccio(servo, angolo):
-	for i in range(POSIZIONI[servo], angolo, -1 if angolo < POSIZIONI[servo] else 1):
-		braccio_raw(servo, i)
-		sleep(0.02)
-	POSIZIONI[servo] = angolo
+	"""Modifica l'angolo di un servo con delay"""
+    for i in range(POSIZIONI[servo], angolo, -1 if angolo < POSIZIONI[servo] else 1):
+        braccio_raw(servo, i)
+        sleep(0.02)
+    POSIZIONI[servo] = angolo
 
-def pinza(apri, chiudi):
-	gpio.output(12, apri)
-	gpio.output(6, chiudi)
+# def pinza(apri, chiudi):
+#	gpio.output(12, apri)
+#	gpio.output(6, chiudi)
+
 
 braccio_raw(1, 80)
 
 if __name__ == "__main__":
-	while True:
-		srv = int(input("Servo: "))
-		agl = int(input("Angolo: "))
-		braccio(srv, agl)
+    while True:
+        srv = int(input("Servo: "))
+        agl = int(input("Angolo: "))
+        braccio(srv, agl)
 
+
+class Pinza():
+    def __init__(self) -> None:
+        setup(6, OUT)
+        setup(16, IN, PUD_DOWN)
+        setup(12, OUT)
+        self.tempo_chiusura: int
+
+    def apri(self) -> None:
+        """Apre la pinza"""
+        output(12, HIGH)
+        output(6, LOW)
+        self.tempo_chiusura = time()
+
+    def chiudi(self) -> None:
+        """Chiude la pinza"""
+        output(12, LOW)
+        output(6, HIGH)
+        # TODO TEST
+        # sleep(self.tempo_chiusura)
+        # self.stop()
+
+    def stop(self) -> None:
+        """Ferma la pinza"""
+        output(12, LOW)
+        output(6, LOW)
+        self.tempo_chiusura = time() - self.tempo_chiusura
+
+    def pulsante_premuto(self) -> None:
+        while gpio_input(16) != HIGH:
+            pass
--- tutto/presentazione_definitiva.py
+++ tutto/presentazione_definitiva.py
@@ -1,29 +1,32 @@
-from braccio_lib import braccio, pinza
-from RPi.GPIO import HIGH, LOW, IN, OUT, PUD_DOWN
+from braccio_lib import braccio, Pinza
 import RPi.GPIO as gpio
 from time import sleep
 from universal import move, s
 from threading import Thread
 
-#1 secondo = 0,74M
+P = Pinza()
 
-#metri = input("METRI: ") 
+# 1 secondo = 0,74M
+
+#metri = input("METRI: ")
 #tempo = metri / 0.74
-gpio.setup(16, IN, PUD_DOWN)
+
 
 cont = 0
 RUN = True
 
+
 def read():
-	while RUN:
-		print("ARDUINO: " + s.readline().decode())
+    while RUN:
+        print("ARDUINO: " + s.readline().decode())
+
 
 Thread(target=read).start()
 
 #input("PREMI INVIO PER PINZA")
-pinza(HIGH, LOW)
+P.apri()
 sleep(2.5)
-pinza(LOW, LOW)
+P.stop()
 
 input("PREMI INVIO PER MOTORI")
 move("dritto", 0)
@@ -31,45 +34,43 @@ sleep(2)
 move("spegni", 0)
 sleep(0.5)
 
-#exit()
+# exit()
 
 try:
     while 1:
-            braccio(1, 170)
+        braccio(1, 170)
 
-            pinza(LOW, HIGH)
-            while gpio.input(16) != HIGH:
-                    pass
-            sleep(0.4)
-            pinza(LOW, LOW)
+        P.chiudi()
+        P.pulsante_premuto()
+        sleep(0.4)
+        P.stop()
 
+        if False:  # input("MATERIALE: ") == 'p':
+            # PLASTICA
+            braccio(1, 40)
+            braccio(3, 0)
+            braccio(15, 70)
+            braccio(8, 100)
+            cont = 1
+        else:
+            # CARTA
+            braccio(1, 40)
+            braccio(3, 60)
+            braccio(15, 70)
+            braccio(8, 100)
+            cont = 0
 
-            if False: #input("MATERIALE: ") == 'p':
-                    #PLASTICA
-                    braccio(1, 40)
-                    braccio(3, 0)
-                    braccio(15, 70)
-                    braccio(8, 100)
-                    cont = 1
-            else:
-                    #CARTA
-                    braccio(1, 40)
-                    braccio(3, 60)
-                    braccio(15, 70)
-                    braccio(8, 100)
-                    cont = 0
+        P.apri()
+        sleep(2.5)
+        P.stop()
 
-            pinza(HIGH, LOW)
-            sleep(2.5)
-            pinza(LOW, LOW)
+        # RESET
+        braccio(15, 0)
+        braccio(8, 20)
+        braccio(3, 30)
 
-            #RESET
-            braccio(15, 0)
-            braccio(8, 20)
-            braccio(3, 30)
-
-            print("PREMI INVIO PER RIAVVIARE")
-            input()
+        print("PREMI INVIO PER RIAVVIARE")
+        input()
 except KeyboardInterrupt:
     RUN = False
 
